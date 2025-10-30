4550d3f57e5b1b6a144435cd3633df29d09334a3
tratteo <bltmtt@gmail.com>
2025-10-30 11:17:49 +0100

    Release 30/10/2025 11:17:49,09

--- app/components/og_image/article.vue
+++ app/components/og_image/article.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="relative h-full w-full flex flex-row items-start gap-4 bg-slate-900 text-white">
+    <div class="relative h-full w-full flex flex-row items-start gap-2 bg-[#1d1d1f] text-white">
         <div class="w-[50%] h-full">
             <div class="p-12 flex flex-col items-stretch h-full justify-start gap-10">
                 <div class="flex-1 flex flex-col gap-4">
                     <p class="text-5xl font-bold">{{ title }}</p>
-                    <p class="text-xl opacity-75">{{ description }}</p>
+                    <p class="text-2xl opacity-75">{{ description }}</p>
                     <div v-if="tags" class="flex items-center flex-row gap-4 w-full">
                         <div v-for="t in tags" class="bg-[#fe9a00] border rounded-md px-2 py-1 text-black font-semibold">{{ t }}</div>
                     </div>
@@ -18,8 +18,8 @@
             </div>
         </div>
         <div class="w-[50%] flex flex-col items-center justify-center h-full">
-            <div class="flex h-[80%] w-full border-2 border-white/20 rounded-l-4xl overflow-hidden relative rounded-xl">
-                <img :src="thumbnail" class="w-full h-full object-cover rounded-l-4xl" />
+            <div class="flex h-[80%] w-full border-2 border-white/20 rounded-l-1rem overflow-hidden relative">
+                <img :src="thumbnail" class="w-full h-full object-cover rounded-l-1rem" />
             </div>
         </div>
     </div>
